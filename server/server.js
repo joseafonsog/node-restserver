7f4795746110686087e2d2fe0676fef0e78479fe
Jose Afonso <jose.afonso.g@outlook.com>
2019-11-04 18:13:51 -0300

    Init google auth configuration

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require("./config/config");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -11,6 +12,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+// public folder
+app.use(express.static(path.resolve(__dirname, "../public")));
 
 // Routes global configuration
 app.use(require("./routes/"));
